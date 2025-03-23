e95d3c82d3d6a2fd05b7cc9f4ddfa32ecfaaa938
Luís Vitor <luis.sg.vitor@gmail.com>
2025-03-23 19:19:16 -0300

    feat: implement cache in gql queries

--- src/modules/pokemons/pokemons.module.ts
+++ src/modules/pokemons/pokemons.module.ts
@@ -1,4 +1,5 @@
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { PokemonsResolver } from 'src/modules/pokemons/resolvers/pokemons.resolver';
 import { PokeApiService } from 'src/modules/pokemons/services/pokeapi.service';
@@ -6,7 +7,7 @@ import { PokemonsService } from 'src/modules/pokemons/services/pokemons.service'
 import { TypesService } from 'src/modules/pokemons/services/types.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register()],
   providers: [PokemonsService, PokemonsResolver, TypesService, PokeApiService],
 })
 export class PokemonsModule {}
--- src/modules/pokemons/resolvers/pokemons.resolver.ts
+++ src/modules/pokemons/resolvers/pokemons.resolver.ts
@@ -1,3 +1,5 @@
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { SortingParams, PaginationParams, FilterParams } from 'src/graphql';
 import {
@@ -8,7 +10,14 @@ import { PokemonsService } from 'src/modules/pokemons/services/pokemons.service'
 
 @Resolver('Pokemon')
 export class PokemonsResolver {
-  constructor(private readonly pokemonsService: PokemonsService) {}
+  protected readonly FIND_MANY_CACHE_KEY_PREFIX = 'findManyPokemonResponse';
+  protected readonly PAGINATE_CACHE_KEY_PREFIX = 'paginatePokemonResponse';
+  protected readonly DEFAULT_CACHE_TTL = 20 * 1000;
+
+  constructor(
+    private readonly pokemonsService: PokemonsService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
 
   @Query()
   async findManyPokemon(
@@ -17,10 +26,24 @@ export class PokemonsResolver {
     @Args('filterParams', { nullable: true })
     filterParams?: FilterParams,
   ) {
-    return this.pokemonsService.list({
+    const cacheKey = this.generateCacheKey(
+      this.FIND_MANY_CACHE_KEY_PREFIX,
+      sortingParams,
+      filterParams,
+    );
+
+    const cachedPokemons = await this.cacheManager.get(cacheKey);
+
+    if (cachedPokemons) return cachedPokemons;
+
+    const pokemons = await this.pokemonsService.list({
       sortingParams,
       filterParams,
     });
+
+    await this.cacheManager.set(cacheKey, pokemons, this.DEFAULT_CACHE_TTL);
+
+    return pokemons;
   }
 
   @Query()
@@ -32,7 +55,18 @@ export class PokemonsResolver {
     @Args('filterParams', { nullable: true })
     filterParams?: FilterParams,
   ) {
-    return this.pokemonsService.list({
+    const cacheKey = this.generateCacheKey(
+      this.PAGINATE_CACHE_KEY_PREFIX,
+      sortingParams,
+      filterParams,
+      paginationParams,
+    );
+
+    const cachedPokemons = await this.cacheManager.get(cacheKey);
+
+    if (cachedPokemons) return cachedPokemons;
+
+    const pokemons = await this.pokemonsService.list({
       paginationParams: {
         skip: paginationParams.perPage * (paginationParams.page - 1),
         take: paginationParams.perPage,
@@ -40,6 +74,37 @@ export class PokemonsResolver {
       sortingParams,
       filterParams,
     });
+
+    await this.cacheManager.set(cacheKey, pokemons, this.DEFAULT_CACHE_TTL);
+
+    return pokemons;
+  }
+
+  private generateCacheKey(
+    prefix: string,
+    sortingParams?: SortingParams,
+    filterParams?: FilterParams,
+    paginationParams?: PaginationParams,
+  ): string {
+    const sortingKey = sortingParams
+      ? `sort:${sortingParams.field}:${sortingParams.direction}`
+      : 'sort:default';
+
+    const filterKeyParts = [];
+    if (filterParams?.name) filterKeyParts.push(`name:${filterParams.name}`);
+
+    if (filterParams?.type) filterKeyParts.push(`type:${filterParams.type}`);
+
+    const filterKey =
+      filterKeyParts.length > 0
+        ? `filter:${filterKeyParts.join(':')}`
+        : 'filter:default';
+
+    const paginationKey = paginationParams
+      ? `page:${paginationParams.page}:perPage:${paginationParams.perPage}`
+      : 'page:none';
+
+    return `${prefix}:${sortingKey}:${filterKey}:${paginationKey}`;
   }
 
   @Mutation()
@@ -47,7 +112,11 @@ export class PokemonsResolver {
     @Args('createPokemonPayload')
     payload: CreatePokemonInput,
   ) {
-    return this.pokemonsService.create(payload);
+    const pokemon = await this.pokemonsService.create(payload);
+
+    await this.cacheManager.clear();
+
+    return pokemon;
   }
 
   @Mutation()
@@ -55,17 +124,28 @@ export class PokemonsResolver {
     @Args('id') id: string,
     @Args('updatePokemonPayload') payload: UpdatePokemonInput,
   ) {
-    return this.pokemonsService.updateById(+id, payload);
+    const pokemon = await this.pokemonsService.updateById(+id, payload);
+
+    await this.cacheManager.clear();
+
+    return pokemon;
   }
 
   @Mutation(() => Boolean)
   async deleteOnePokemon(@Args('id') id: string): Promise<boolean> {
     await this.pokemonsService.deleteById(+id);
+
+    await this.cacheManager.clear();
+
     return true;
   }
 
   @Mutation()
   async importPokemonById(@Args('id') id: string) {
-    return this.pokemonsService.importPokemonById(+id);
+    const pokemon = await this.pokemonsService.importPokemonById(+id);
+
+    await this.cacheManager.clear();
+
+    return pokemon;
   }
 }
